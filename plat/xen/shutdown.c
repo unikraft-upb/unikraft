d7a1aaa4efa1c200df51a4c8b1b4af6a58b03512
Simon Kuenzer <simon.kuenzer@neclab.eu>
2021-12-07 01:21:43 +0100

    plat/xen: Shutdown request handler
    Starts a handler thread that waits for request via the
    XenStore entry `control/shutdown`.
    Please note that in this commit we call directly
    `ukplat_terminate()`. As soon as we are ready with proper
    shutdown support, we should forward the request to the
    application and general libraries so that they can take
    care of all needed actions for a clean shutdown (e.g.,
    clean filesystem unmount).
    The handler logic is derived from NEC Labs clone of Mini-OS:
    https://github.com/sysml/mini-os/blob/master/shutdown.c
    
    Signed-off-by: Simon Kuenzer <simon.kuenzer@neclab.eu>

--- plat/xen/shutdown.c
+++ plat/xen/shutdown.c
@@ -4,6 +4,8 @@
  *
  *
  * Copyright (c) 2017, NEC Europe Ltd., NEC Corporation. All rights reserved.
+ * Copyright (c) 2021, NEC Laboratories Europe GmbH, NEC Corporation.
+ *                     All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -35,7 +37,15 @@
 #include <string.h>
 #include <uk/arch/lcpu.h>
 #include <uk/plat/bootstrap.h>
+#include <uk/errptr.h>
 #include <errno.h>
+#if CONFIG_XEN_SHUTDOWN
+#include <uk/init.h>
+#include <uk/sched.h>
+#include <xenbus/xs.h>
+#include <xenbus/client.h>
+#include <stdlib.h>
+#endif /* CONFIG_XEN_SHUTDOWN */
 
 #include <xen/xen.h>
 #include <common/console.h>
@@ -54,12 +64,15 @@ void ukplat_terminate(enum ukplat_gstate request)
 
 	switch (request) {
 	case UKPLAT_HALT:
+		uk_pr_info("Powering off...\n");
 		reason = SHUTDOWN_poweroff;
 		break;
 	case UKPLAT_RESTART:
+		uk_pr_info("Rebooting...\n");
 		reason = SHUTDOWN_reboot;
 		break;
 	default: /* UKPLAT_CRASH */
+		uk_pr_info("Crashing...\n");
 		reason = SHUTDOWN_crash;
 		break;
 	}
@@ -81,3 +94,91 @@ int ukplat_suspend(void)
 	/* ret = HYPERVISOR_suspend(virt_to_mfn(start_info_ptr)); */
 	return -ret;
 }
+
+#if CONFIG_XEN_SHUTDOWN
+static void shutdown_handler(void *argp)
+{
+	struct xenbus_watch *watch = (struct xenbus_watch *) argp;
+	char *req;
+	int err;
+
+	UK_ASSERT(watch);
+
+	for (;;) {
+		xenbus_watch_wait_event(watch);
+		req = xs_read(XBT_NIL, "control", "shutdown");
+		if (PTRISERR(req)) {
+			uk_pr_debug("Ignore read error of incoming request: %d\n",
+				    PTR2ERR(req));
+			continue;
+		}
+		if (!strcmp(req, "")) {
+			/* FIXME: Investigate reason of spurious events */
+			uk_pr_debug("Ignore spurious event\n");
+			free(req);
+			continue;
+		}
+
+		/* Acknowledge request by clearing the node */
+		/* TODO: Is this really needed? */
+		err = xs_write(XBT_NIL, "control", "shutdown", "");
+		if (err) {
+			/* Ignore failure but throw a message */
+			uk_pr_err("Failed to acknowledge '%s' request: %d\n",
+				  req, err);
+		}
+
+		/*
+		 * Take action
+		 * TODO: We call here directly `ukplat_terminate()`. However,
+		 *       as soon as we are ready, we should forward a signal
+		 *       to our application/general libraries so that it can
+		 *       take all needed actions for a clean shutdown (e.g.,
+		 *       clean filesystem unmount). We should only receive
+		 *       each request here and forward it.
+		 */
+		if (!strcmp(req, "poweroff")) {
+			uk_pr_info("Shutdown requested\n");
+			ukplat_halt();
+		} else if (!strcmp(req, "reboot")) {
+			uk_pr_info("Restart requested\n");
+			ukplat_restart();
+		} else {
+			uk_pr_err("Ignore unsupported request: %s\n",
+				  req);
+		}
+		free(req);
+	}
+}
+
+static int shutdown_handler_init(void)
+{
+	struct xenbus_watch *watch;
+	struct uk_thread *t;
+	int ret = 0;
+
+	watch = xs_watch_path(XBT_NIL, "control/shutdown");
+	if (PTRISERR(watch)) {
+		uk_pr_crit("Failed to create watch for 'control/shutdown': %d\n",
+			   PTR2ERR(watch));
+		ret = -PTR2ERR(watch);
+		goto err_out;
+	}
+
+	t = uk_thread_create("xen-shutdown",
+			     shutdown_handler, watch);
+	if (!t) {
+		uk_pr_crit("Failed to create shutdown handler thread\n");
+		ret = -ENOMEM;
+		goto err_unwatch;
+	}
+	return 0;
+
+err_unwatch:
+	xs_unwatch(XBT_NIL, watch);
+err_out:
+	return ret;
+}
+
+uk_plat_initcall(shutdown_handler_init);
+#endif /* CONFIG_XEN_SHUTDOWN */
